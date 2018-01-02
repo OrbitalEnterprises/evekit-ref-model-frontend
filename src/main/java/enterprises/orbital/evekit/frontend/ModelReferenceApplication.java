f06ec4b20e918f17bfa6b092a123c753c7544182
deadlybulb <deadlybulb@orbital.enterprises>
2018-01-01 20:50:44 -0500

    prep for 4.1.0 release

--- src/main/java/enterprises/orbital/evekit/frontend/ModelReferenceApplication.java
+++ src/main/java/enterprises/orbital/evekit/frontend/ModelReferenceApplication.java
@@ -3,9 +3,10 @@ package enterprises.orbital.evekit.frontend;
 import enterprises.orbital.base.OrbitalProperties;
 import enterprises.orbital.base.PersistentProperty;
 import enterprises.orbital.db.DBPropertyProvider;
-import enterprises.orbital.evekit.ws.eve.ModelEveWS;
-import enterprises.orbital.evekit.ws.map.ModelMapWS;
+import enterprises.orbital.evekit.ws.alliance.ModelAllianceWS;
+import enterprises.orbital.evekit.ws.faction.ModelFactionWS;
 import enterprises.orbital.evekit.ws.server.ModelServerWS;
+import enterprises.orbital.evekit.ws.sov.ModelSovWS;
 
 import javax.ws.rs.core.Application;
 import java.io.IOException;
@@ -15,8 +16,8 @@ import java.util.Set;
 public class ModelReferenceApplication extends Application {
   // Property which holds the name of the persistence unit for properties
   public static final String PROP_PROPERTIES_PU = "enterprises.orbital.evekit.ref-model-frontend.properties.persistence_unit";
-  public static final String PROP_APP_PATH      = "enterprises.orbital.evekit.ref-model-frontend.apppath";
-  public static final String DEF_APP_PATH       = "http://localhost/ref-model";
+  public static final String PROP_APP_PATH = "enterprises.orbital.evekit.ref-model-frontend.apppath";
+  public static final String DEF_APP_PATH = "http://localhost/ref-model";
 
   public ModelReferenceApplication() throws IOException {
     // Populate properties
@@ -27,11 +28,12 @@ public class ModelReferenceApplication extends Application {
 
   @Override
   public Set<Class<?>> getClasses() {
-    Set<Class<?>> resources = new HashSet<Class<?>>();
+    Set<Class<?>> resources = new HashSet<>();
     // Model API resources
+    resources.add(ModelAllianceWS.class);
+    resources.add(ModelFactionWS.class);
     resources.add(ModelServerWS.class);
-    resources.add(ModelEveWS.class);
-    resources.add(ModelMapWS.class);
+    resources.add(ModelSovWS.class);
     // Swagger additions
     resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
     resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
